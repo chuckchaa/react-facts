748bd425103afd947720175c68a66c58c86afe8e
Maks <115740657+chuckchaa@users.noreply.github.com>
2023-02-24 19:47:01 +0200

    Add files via upload

---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function NavBar() {
+    return (
+        <nav>
+            <img src="images/logo.png" className="nav--icon"/>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    );
+}
